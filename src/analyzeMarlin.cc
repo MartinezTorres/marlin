49c15de11c6cecdb90e62c24bd2bedacd1204284
Manuel Martinez <manuel.martinez@kit.edu>
2017-10-06 16:05:29 +0200

    whaaaaat nodes

--- src/analyzeMarlin.cc
+++ src/analyzeMarlin.cc
@@ -7,7 +7,11 @@ using namespace std;
 int main() {
 	
 	ofstream tex("out.tex");
-	tex << "\\documentclass{article}" << endl << "\\usepackage[a4paper, margin=1cm]{geometry}" << endl << "\\usepackage{tikz}" << endl << "\\usepackage{pgfplots}" << endl << "\\begin{document}" << endl;	
+	tex << "\\documentclass{article}" << endl 
+		<< "\\usepackage[a4paper, margin=1cm]{geometry}" << endl 
+		<< "\\usepackage{tikz}" << endl 
+		<< "\\usepackage{pgfplots}" << endl 
+		<< "\\begin{document}" << endl;	
 
 	/*tex << "\\newcommand{\\customChartSize}{height=3cm, width=5cm,}" << endl;
 
@@ -63,6 +67,59 @@ int main() {
 	
 	// Speed vs efficiency, Overlap  + deduplication.
 	if (true) {
+
+		tex << R"ML(
+		\begin{figure}
+		\centering
+		\begin{tikzpicture} 
+		\begin{semilogxaxis}[
+			title="Decoding Speed vs Efficiency", 
+			title style={yshift=-1mm},
+			height=3cm, width=5cm,
+			log origin=infty, 
+			log ticks with fixed point, 
+			scale only axis, 
+			enlargelimits=false, 
+			ymin=0, ymax=100, 
+			xmin=0.021544, xmax=46.416, 
+			ymajorgrids, major grid style={dotted, gray}, 
+%			xtick=data,
+			x tick label style={font={\footnotesize},yshift=1mm}, 
+			y tick label style={font={\footnotesize},xshift=-1mm},
+			ylabel={\emph{Efficiency}}, 
+			xlabel={\emph{GiB/s}}, 
+%			ylabel style={font={\footnotesize},yshift=4mm}, 
+%			xlabel style={font={\footnotesize},yshift=5.25mm, xshift=29mm},
+			axis y line=left
+			])ML";
+			
+		tex << R"ML(
+			\addplot[color=red,mark=x] coordinates {
+				(0.1,95)
+				(0.2,90)
+				(0.3,91)
+				(0.4,92)
+			};
+			)ML";
+
+		tex << R"ML(
+			\legend{Dedup, No Dedup}
+			)ML";
+			
+			
+		tex << R"ML(
+			\end{semilogxaxis} 
+			\end{tikzpicture}
+			\caption{}
+			\label{fig:}
+			\end{figure}
+			)ML";
+		
+//		Marlin2018Simple::overrideConfiguration("dedup",0.);
+//		Marlin2018Simple(P,options["--keySize"],options["--overlap"],options["--maxWordSize"]).test(P,options["--testSize"]);
+	}
+	
+	if (false) {
 
 		tex << R"ML(
 		\begin{tikzpicture} 
