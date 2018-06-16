fa754a204ffaf99cd6e44bed264c7cf1a0568cd9
Manuel Martinez <manuel.martinez@kit.edu>
2018-06-16 10:10:16 +0200

    added the default configuration file

---
+++ src/configuration.cc
@@ -0,0 +1,75 @@
+/***********************************************************************
+
+Marlin: A Fast Entropy Codec
+
+MIT License
+
+Copyright (c) 2018 Manuel Martinez Torres
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+***********************************************************************/
+
+#include "dictionary.hpp"
+
+
+std::map<std::string, double> MarlinDictionary::updateConf( 
+	const std::map<MarlinDictionary::SourceSymbol, double> &symbols, 
+	MarlinDictionary::Configuration conf) {
+	
+	conf.emplace("K",8);
+	conf.emplace("O",2);
+	
+	conf.emplace("debug",1);
+	conf.emplace("purgeProbabilityThreshold",1e-5);
+	conf.emplace("iterations",3);
+	conf.emplace("minMarlinSymbols", std::max(1U<<size_t(conf.at("O")),8U));
+	conf.emplace("maxMarlinSymbols",(1U<<size_t(conf.at("K")))-1);
+		
+	if (not conf.count("shift")) {
+		conf["shift"] = 0;
+		double best = MarlinDictionary(symbols, conf).efficiency;
+		for (int s=1; s<6; s++) {
+			conf["shift"] = s;
+			double e = MarlinDictionary(symbols, conf).efficiency;
+			if (e<=best) {
+				conf["shift"] = s-1;
+				break;
+			}
+			best = e;
+		}
+	}
+	
+	if (not conf.count("maxWordSize")) {
+		conf["maxWordSize"] = 15;
+		double e15 = MarlinDictionary(symbols, conf).efficiency;
+		conf["maxWordSize"] = 7;
+		double e7 = MarlinDictionary(symbols, conf).efficiency;
+		conf["maxWordSize"] = 3;
+		double e3 = MarlinDictionary(symbols, conf).efficiency;
+		if (e7>1.0001*e3) {
+			conf["maxWordSize"] = 7;
+		}
+		if (e15>1.0001*e7) {
+			conf["maxWordSize"] = 15;
+		}
+	}
+	
+	return conf;
+}
