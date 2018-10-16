0e1e852307604a59af9b8f4b1e2be7e435a23c2d
Manuel Martinez <manuel.martinez@kit.edu>
2018-10-16 15:46:18 +0200

    adding the template for the image file

---
+++ examples/marlinImage.cc
@@ -0,0 +1,14 @@
+#include <marlin.h>
+
+#include <iostream>
+#include <opencv/cv.h>
+
+int main(int argc, char **argv) {
+	
+	
+	std::make_shared<Marlin2019>(Distribution::Laplace)
+	
+	
+	
+	return 0;
+}
